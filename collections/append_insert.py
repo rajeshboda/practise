b7bd12b89c55eee5a3e779a821cff88e0e3539e6
Rajesh Boda <rajesh.boda@sap.com>
2018-12-16 22:00:57 +0530

    Multi dimensional list

---
+++ collections/append_insert.py
@@ -0,0 +1,15 @@
+empty_list =[]
+
+empty_list.append("1")
+empty_list.append("2")
+
+for it in empty_list:
+    print(it)
+
+empty_list.insert(0,0)
+empty_list.insert(15,43)
+
+print(".......")
+
+for it in empty_list:
+    print(it)
---
+++ collections/hetroList.py
@@ -0,0 +1,3 @@
+hetro_List =["Rajesh","Boda",1,["Java",":"]]
+for itr in hetro_List:
+    print(itr, sep="**")
