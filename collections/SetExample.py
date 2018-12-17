8f1059a6ffe3ce52892d75b2ae6daa2580336156
Rajesh Boda <rajesh.boda@sap.com>
2018-12-17 20:13:54 +0530

    Set exmaple V1

--- collections/SetExample.py
+++ collections/SetExample.py
@@ -1,5 +1,29 @@
-mySet= {"Java", "C","C++","Python"}
+mySet = {"Java", "C", "C++", "Python......".rstrip('.'), "Python"} ## no duplicates
 print(mySet)
 
+#for it in mySet:
+#    print(it)
+
+print("Size -->", len(mySet))
+mySet.add("Crazy Programming Languages")
+mySet.add("But rocks!!!")
+
+#for it in mySet:
+#    print(it)
+
+copyMySet = mySet
+print(id(mySet))
+print(id(copyMySet))
+
+mySet.add("But rocks!!! again")
+
 for it in mySet:
+    print(it)
+
+
+for it in copyMySet:
+    print(it)
+
+unionOfAll = mySet.union(copyMySet)
+for it in unionOfAll:
     print(it)
