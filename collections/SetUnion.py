dc4da077325f98fbc89d56a490a25698acf22037
Rajesh Boda <rajesh.boda@sap.com>
2018-12-17 20:18:42 +0530

    Set union_intersection

---
+++ collections/SetUnion.py
@@ -0,0 +1,13 @@
+setOne = {"1", "2", "3", "4"}
+print(setOne)
+
+setTwo ={"4", "1", "7", "9"}
+print(setTwo)
+
+unionOfAll = setOne.union(setTwo)
+print(unionOfAll)
+
+interSectionOfAll = setOne.intersection(setTwo)
+print(interSectionOfAll)
+
+print(setOne.difference(setTwo))
