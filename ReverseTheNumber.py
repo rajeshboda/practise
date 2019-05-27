ca0c1a94fb75cd66a2b5e02a7455a1808c37a679
Rajesh Boda <rajeshboda@gmail.com>
2019-05-27 12:20:09 +0530

    initial commit

--- ReverseTheNumber.py
+++ ReverseTheNumber.py
@@ -1,5 +1,5 @@
 # this snippet reverse the given number and prints it.
-numberToReverse = int(input("Please enter the number to reverse -->  "))
+numberToReverse = int(input("Please enter the number to reverse -->  should be a number"))
 while numberToReverse > 0:
     print(numberToReverse % 10, end='')
     numberToReverse //= 10
